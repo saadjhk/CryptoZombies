04d5a0dea5fd0e4d00b374aafb37dcbde2904665
Saad Ahmed Siddiqui <48211799+saadjhk@users.noreply.github.com>
2022-02-04 04:54:48 +0500

    compileable

---
+++ contracts/ZombieFactory.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string _name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name) private {
+        uint dna = _generateZombieDna(_name);
+        zombies.push(Zombie(_name, dna));
+    }
+
+    function _generateZombieDna(string memory _name) private view returns (uint) {
+        // fix this later
+        // uint rand = uint(abi.encodePacked(_name));
+        return 0;
+    }
+
+    function generateRandomZombie(string memory _name) public {
+        _createZombie(_name);
+    }
+
+}
