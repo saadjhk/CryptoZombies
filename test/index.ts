0512b5ee9e70136438895184185c4db25fd84a6e
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-02-08 20:14:21 +0500

    f

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("ZombieFactory", function () {
-  it("Should return an arbitrary number", async function () {
+  it("Should create a zombie", async function () {
     const ZFactory = await ethers.getContractFactory("ZombieFactory");
     const zFactory = await ZFactory.deploy();
     await zFactory.deployed();
