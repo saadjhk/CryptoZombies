d7ee7da6dea7c0499b295f21486bafc73678c345
Saad Ahmed Siddiqui <48211799+saadjhk@users.noreply.github.com>
2022-03-07 17:29:21 +0500

    nl

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -1,8 +1,11 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./zombiefactory.sol";
+import "./Zombiefactory.sol";
+import "./KittyInterface.sol";
 
 contract ZombieFeeding is ZombieFactory {
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
@@ -12,4 +15,9 @@ contract ZombieFeeding is ZombieFactory {
     _createZombie("NoName", newDna);
   }
 
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna);
+  }
 }
